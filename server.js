13dd364a68b6340782da08ff47ee957e7017d6a5
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-12-12 16:11:50 -0600

    added addBooks function

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const mongoose = require('mongoose');
-const express = require('express');
-const cors = require('cors');
-const Books = require('./models/books.js');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const Books = require("./models/books.js");
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
-mongoose.connect( process.env.MONGODB_URL );
+mongoose.connect(process.env.MONGODB_URL);
 
-app.get('/books', handleGetBooks);
+app.get("/books", handleGetBooks);
+app.post("/books", addBooks);
 
-async function handleGetBooks( request, response){
- try{
-   const books = await Books.find();
-    response.status(200).json(books)
+async function addBooks(request, response) {
+  let book = request.body;
+
+  let addedBook = await Books.create(book);
+
+  response.json(addedBook);
+}
+
+async function handleGetBooks(request, response) {
+  try {
+    const books = await Books.find();
+    response.status(200).json(books);
     console.log(books);
-   
-} catch(e) {
-  console.error('Error getting books:', e);
-  response.status(500).json({err: 'Internal Server Error'});
+  } catch (e) {
+    console.error("Error getting books:", e);
+    response.status(500).json({ err: "Internal Server Error" });
+  }
 }
-}
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
+app.get("/test", (request, response) => {
+  response.send("test request received");
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
